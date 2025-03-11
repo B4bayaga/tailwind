4719672bc9790b06467823d1c4c0772863d2e77c
Rafel Jesus <rafael@enotebook.com>
2025-03-11 16:22:56 -0300

    Criado repositório.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#fd7043',
+                primaryDark: '#e85f33',
+                primaryLight: '#fe8c66',
+            },
+        },
+    },
+}
